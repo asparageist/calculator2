4d7dc6075c22a0ed2d189804cbff29bc5c877a2a
Joseph Murray <josephwmurray@gmail.com>
2023-06-14 19:49:18 -0700

    add functionality, add AI generated experiment HTML

--- js/scripts.js
+++ js/scripts.js
@@ -18,7 +18,22 @@ function divide(num1, num2) {
 // User Interface Logic
 function handleCalculation(event) {
   event.preventDefault();
-  // the code to get and process form values will go here!
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
+
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
+
+    document.getElementById("output").innerText = result;
 }
 
 window.addEventListener("load", function() {
